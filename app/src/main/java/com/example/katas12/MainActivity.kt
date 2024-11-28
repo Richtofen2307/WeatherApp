233efefc1f042ac282fd54211f94821ce2798324
juanes1802 <menajuanes945@gmail.com>
2024-11-28 06:51:30 -0500

    ya la aplicaion ejecuta falta correjir temas de el api

--- app/src/main/java/com/example/katas12/MainActivity.kt
+++ app/src/main/java/com/example/katas12/MainActivity.kt
@@ -24,7 +24,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.pager.*
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
